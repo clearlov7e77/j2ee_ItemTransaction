07376642b198aeaee35505d469fac19a66f86e79
clearlov7e77 <646518628@qq.com>
2020-09-16 16:28:57 +0800

    fuck

---
+++ src/main/java/com/example/mizore/MizoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.mizore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MizoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MizoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mizore/MizoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mizore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MizoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
