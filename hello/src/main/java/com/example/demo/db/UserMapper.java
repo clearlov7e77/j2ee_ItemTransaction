f0d006232cf8200b0bc41a6cc11e62a45930473b
clearlov7e77 <646518628@qq.com>
2020-09-16 16:41:53 +0800

    del

--- hello/src/main/java/com/example/demo/db/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.db;
-import com.example.demo.bean.User;
-public interface UserMapper{
-    public User getUser(String name);
-    public int  insertUser(User usr);
-    public int  deleteUser(String name);
-
-}
